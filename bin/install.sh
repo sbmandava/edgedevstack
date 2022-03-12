cfad4bf185e5c0526f13b463a189ca1a7030c9ec
Suresh <vdesibabu@gmail.com>
2022-03-12 10:14:50 -0600

    update

--- bin/install.sh
+++ bin/install.sh
@@ -8,11 +8,16 @@ install_desktop () {
 update_env 	
 apt-get install -y lubuntu-desktop xrdp
 apt-get remove -y --purge libreoffice* transmission* vlc* 2048* skanlite* bluedevil* trojita* qpdfview* pulseaudio* ark* byobu* feather* kcalc*
-apt-get remove -y --purge qlipper* compton* noblenote* *speech* *gphoto2* *modem*
+apt-get remove -y --purge qlipper* compton* noblenote* *speech* *gphoto2* *modem* lximage* usb-creator* qps*
 apt-get clean
 apt-get -y autoremove
 }
 
+install_vscode () {
+cd /tmp
+wget -O /tmp/vs-code.dpkg https://go.microsoft.com/fwlink/?LinkID=760868;dpkg -i /tmp/vs-code.dpkg 
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+}
 
 #### main
 if [ `whoami` != root ]; then

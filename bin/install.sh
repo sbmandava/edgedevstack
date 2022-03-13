9b6bacc89c37f76116b7952f6f4c2ab809e98b0d
Suresh <vdesibabu@gmail.com>
2022-03-12 21:31:54 -0600

    update

--- bin/install.sh
+++ bin/install.sh
@@ -8,15 +8,17 @@ install_desktop () {
 update_env 	
 apt-get install -y lubuntu-desktop xrdp
 apt-get remove -y --purge libreoffice* transmission* vlc* 2048* skanlite* bluedevil* trojita* qpdfview* pulseaudio* ark* byobu* feather* kcalc*
-apt-get remove -y --purge qlipper* compton* noblenote* *speech* *gphoto2* *modem* lximage* usb-creator* qps*
+apt-get remove -y --purge qlipper* compton* noblenote* *speech* *gphoto2* *modem* lximage* usb-creator* qps* *fcitx*
 apt-get clean
 apt-get -y autoremove
 }
 
 install_vscode () {
 cd /tmp
-wget -O /tmp/vs-code.dpkg https://go.microsoft.com/fwlink/?LinkID=760868;dpkg -i /tmp/vs-code.dpkg 
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+snap install code --classic
+snap install helm3 
+snap install kubectl --classic
+snap install kontena-lens --classic
 }
 
 #### main
